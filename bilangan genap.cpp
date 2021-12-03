40c572a81487b3f5f7e53984757e298e73e99b84
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-12-03 21:38:22 +0700

    Add files via upload

---
+++ bilangan genap.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <Conio.h> 
+using namespace std ; 
+
+  int main () { 
+  
+cout << "Bilangan genap \n" << endl ; 
+
+
+	for ( int n = 0 ; n <= 15 ; n ++ ) 
+		if ( n % 2 == 0 ) { 
+		cout << n << " " ; 
+	} else { 
+
+	} 
+}	
+
